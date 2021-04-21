27655023b38a71c0f60b25a530e6a4f947889628
Kaveri_Arkasali <68460091+kaveri-ark@users.noreply.github.com>
2021-04-21 11:53:28 +0530

    update

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var btntranslate = document.querySelector('#btn-primary');
+var btntranslate = document.querySelector('.btn-primary');
+var inputtextarea = document.querySelector('#text-input');
+var converttext = document.querySelector('#inputtext'); 
 
 var urltranslation = "https://api.funtranslations.com/translate/";
 
-btntransalte.addEventListener(click , function(){
-    console.log("click")
-} )
+
+
+
+
+
+
+
+
+btntransalte.addEventListener(click , function());
