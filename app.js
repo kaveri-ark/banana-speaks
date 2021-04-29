61b40efd08f6b15dd2cc262d32ac82994e0c7867
Kaveri_Arkasali <68460091+kaveri-ark@users.noreply.github.com>
2021-04-29 13:35:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ var btntranslate = document.querySelector('.btn-primary');
 var inputtextarea = document.querySelector('#text-input');
 var converttext = document.querySelector('#inputtext'); 
 
-var urltranslation = "https://api.funtranslations.com/translate/";
+var translatedText = "https://api.funtranslations.com/translate/";
 
+function getUrltranslation(input){
+    return serverURL +"?" +"text="+input
+}
 
+function errorHandler(error){
+    console.log("error occur",error)
+    alert("something went wrong please try again later..");
+}
 
+function clickHandler(){
+    var inputtextarea = txtInput.value;
 
+    fetch(getUrltranslation(inputtextarea))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText =json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+}
 
-
-
-
-
-btntransalte.addEventListener(click , function());
+btntransalte.addEventListener(click , clickHandler);
